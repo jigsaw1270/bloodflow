9cc361ac8d5d307a4c6b0fe5adf4cd458db2969c
jigsaw1270 <nomadshomoy@gmail.com>
2024-10-25 17:32:35 +0600

    first commit

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCn2z7ZMXFQ7dKtIJ2M4_uwYMXMmjwGu3s",
+  authDomain: "bloodflow-edaa2.firebaseapp.com",
+  projectId: "bloodflow-edaa2",
+  storageBucket: "bloodflow-edaa2.appspot.com",
+  messagingSenderId: "781415876925",
+  appId: "1:781415876925:web:29c12d09aea3ce52031d54"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
